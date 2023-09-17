0d5c78d44a45c715072a09cd8d9a1ceb4e4d2eda
mcalisadeghi <ali.ke970@gmail.com>
2023-09-17 16:10:23 +0330

    add assets and build login page

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LoginPagess extends StatelessWidget {
+  const LoginPagess({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
